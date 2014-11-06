2c28e8441c49c0b9e2ae895e7ebf6d7b121a060e
JureZ <Jure.Zilavec@virtua-it.si>
2014-11-06 17:29:33 +0100

    New commit for branch 01 - after hotfix made

--- hello.py
+++ hello.py
@@ -1,5 +1,25 @@
-target = open("test.txt", "a")
+target = "";
 target.write("hey wats up from py code, dzureee... ")
 target.write("\n")
 target.close()
 #print "hello from pyhton DZUREEEE... "
+
+# target = open("test.txt", "a")
+target.write("hey wats up from py code, dzureee... ")
+target.write("\n")
+target.close()
+#print "hello from pyhton DZUREEEE... "
+
+# target = open("test.txt", "a")
+target.write("hey wats up from py code, dzureee... ")
+target.write("\n")
+target.close()
+#print "hello from pyhton DZUREEEE... "
+
+# target = open("test.txt", "a")
+target.write("hey wats up from py code, dzureee... ")
+target.write("\n")
+target.close()
+#print "hello from pyhton DZUREEEE... "
+
+# for branch 01 - new commit
