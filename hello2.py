37d446411bdf1b5c2f6f6820d097046e33cba55c
JureZ <Jure.Zilavec@virtua-it.si>
2014-11-06 17:18:10 +0100

    Commit 1 for branch02

--- hello2.py
+++ hello2.py
@@ -5,8 +5,5 @@ target.close()
 #print "hello from pyhton DZUREEEE... "
 
 target = open("test.txt", "a")
-target.write("hey wats up from py code, dzureee... ")
-target.write("\n")
-target.close()
-#print "hello from pyhton DZUREEEE... "
+
 
